5c2fe4130d9ad4b9a8f64a38ea109e43aabbb946
cjee21 <77721854+cjee21@users.noreply.github.com>
2025-11-13 20:27:41 +0800

    Reorganize files + October 2025 update

--- Show UEFI PK, KEK, DB and DBX.cmd
+++ Show UEFI PK, KEK, DB and DBX.cmd
@@ -1,5 +1,5 @@
 :: Created by github.com/cjee21
 @title Show UEFI PK, KEK, DB and DBX
-@powershell -ExecutionPolicy Bypass -Command "& Import-Module '%~dp0Get-UEFIDatabaseSignatures.ps1'; $FormatEnumerationLimit = -1; Write-Output "PK:"; Get-SecureBootUEFI -Name pk | Get-UEFIDatabaseSignatures | Format-List; Write-Output "KEK:"; Get-SecureBootUEFI -Name kek | Get-UEFIDatabaseSignatures | Format-List; Write-Output "DB:"; Get-SecureBootUEFI -Name db | Get-UEFIDatabaseSignatures | Format-List;  Write-Output "DBX:"; Get-SecureBootUEFI -Name dbx | Get-UEFIDatabaseSignatures | Format-List"
+@powershell -ExecutionPolicy Bypass -Command "& Import-Module '%~dp0ps\Get-UEFIDatabaseSignatures.ps1'; $FormatEnumerationLimit = -1; Write-Output "PK:"; Get-SecureBootUEFI -Name pk | Get-UEFIDatabaseSignatures | Format-List; Write-Output "KEK:"; Get-SecureBootUEFI -Name kek | Get-UEFIDatabaseSignatures | Format-List; Write-Output "DB:"; Get-SecureBootUEFI -Name db | Get-UEFIDatabaseSignatures | Format-List;  Write-Output "DBX:"; Get-SecureBootUEFI -Name dbx | Get-UEFIDatabaseSignatures | Format-List"
 @echo.
 @pause
